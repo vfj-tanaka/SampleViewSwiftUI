c2006cd73ee20f7a69f8839b7bb6fee4b4fdc0f7
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-09-28 15:25:42 +0900

    Initial Commit

---
+++ SampleViewSwiftUI/SampleViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleViewSwiftUIApp.swift
+//  SampleViewSwiftUI
+//
+//  Created by mtanaka on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct SampleViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
