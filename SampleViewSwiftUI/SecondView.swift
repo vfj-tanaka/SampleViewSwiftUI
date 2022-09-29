7a93a922932c4967f18b900daf8b9b3f0de528a9
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-09-29 17:13:02 +0900

    TabView

---
+++ SampleViewSwiftUI/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  SampleViewSwiftUI
+//
+//  Created by mtanaka on 2022/09/29.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
