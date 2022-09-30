636d7b2b29bcabd9080c5377662d5d4c3e994733
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-09-30 16:56:28 +0900

    プロフィール画面のサンプル

--- SampleViewSwiftUI/SecondView.swift
+++ SampleViewSwiftUI/SecondView.swift
@@ -7,9 +7,114 @@
 
 import SwiftUI
 
+struct Symbol: Identifiable {
+    var id = UUID()
+    var image: String
+    var name: String
+}
+
 struct SecondView: View {
+    
+    private var frameWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                
+                VStack(spacing: 20) {
+                    
+                    Image("noImage")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
+                        .cornerRadius(75)
+                    
+                    Text("Tanaka Makoto")
+                        .font(.system(size: 20, weight: .semibold))
+                    
+                    Text("サンプルああああああああああああああああああ")
+                }
+                .padding(20)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(1..<20) { _ in
+                            Circle()
+                                .frame(width: 60)
+                                .foregroundColor(.purple)
+                        }
+                    }
+                }
+                
+                let symbols = [Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"), Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"), Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"), Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"), Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"),Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"),Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"),Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"),Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"),Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"),Symbol(image: "drop.fill", name: "drop"),
+                               Symbol(image: "flame.fill", name: "flame"),
+                               Symbol(image: "bolt.fill", name: "bolt"),
+                               Symbol(image: "leaf.fill", name: "leaf"),
+                               Symbol(image: "hare.fill", name: "hare"),
+                               Symbol(image: "tortoise.fill", name: "tortoise")
+                ]
+                // 1行あたり3つのアイコンを表示する
+                let columns: [GridItem] = Array(repeating: .init(.flexible(minimum: 80)), count: 3)
+                
+                LazyVGrid(columns: columns, spacing: 20, pinnedViews: .sectionHeaders) {
+                    Section(header:
+                                HStack {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "playstation.logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: frameWidth / 2, height: 30)
+                                .foregroundColor(.black)
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "xbox.logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: frameWidth / 2, height: 30)
+                                .foregroundColor(.black)
+                        }
+                    }
+                        .frame(width: frameWidth, height: 50)
+                    ) {
+                        ForEach(symbols) { symbol in
+                            VStack {
+                                Image(systemName: symbol.image)
+                                Text(symbol.name)
+                            }
+                        }
+                    }
+                    .background(.white)
+                }
+            }
+        }
     }
 }
 
